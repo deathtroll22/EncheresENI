8cc2433b6d05bebbb9a160baff5b72228fa96ae6
UNiK <UNiK@UNIK-PSE06UQOK4.home>
2023-08-28 16:59:25 +0200

    bll

--- src/main/java/fr/eni/enchereseni/bll/AuctionManager.java
+++ src/main/java/fr/eni/enchereseni/bll/AuctionManager.java
@@ -1,36 +1,42 @@
 package fr.eni.enchereseni.bll;
 import java.util.List;
 
+import fr.eni.enchereseni.bo.User;
+
 public interface AuctionManager {
     // Gestion des enchères
-    void createAuction(Auction auction);
-    void updateAuction(Auction auction);
-    void deleteAuction(int auctionId);
-    Auction getAuctionById(int auctionId);
-    List<Auction> getAuctionsBySeller(String sellerName);
-    List<Auction> getAuctionsByName(String itemName);
-    List<Auction> getAllAuctions();
-    void placeBid(int auctionId, double bidAmount, String bidderName);
-    void closeAuction(int auctionId);
-    List<Auction> getActiveAuctions();
-    List<Auction> getClosedAuctions();
-    List<Auction> getWonAuctionsByUser(String username);
-    List<Bid> getBidsForAuction(int auctionId);
-    List<Bid> getUserBids(String username);
-    Bid getHighestBidForAuction(int auctionId);
-    List<Auction> getUserActiveAuctions(String username);
+    //void createAuction(Auction auction);
+    //void updateAuction(Auction auction);
+    //void deleteAuction(int auctionId);
+    //Auction getAuctionById(int auctionId);
+    //List<Auction> getAuctionsBySeller(String sellerName);
+    //List<Auction> getAuctionsByName(String itemName);
+    //List<Auction> getAllAuctions();
+    //void placeBid(int auctionId, double bidAmount, String bidderName);
+    //void closeAuction(int auctionId);
+    //List<Auction> getActiveAuctions();
+    //List<Auction> getClosedAuctions();
+    //List<Auction> getWonAuctionsByUser(String username);
+    //List<Bid> getBidsForAuction(int auctionId);
+    //List<Bid> getUserBids(String username);
+    //Bid getHighestBidForAuction(int auctionId);
+    //List<Auction> getUserActiveAuctions(String username);
 
     // Gestion des statuts d'article
-    void markItemAsForSale(int auctionId);
-    void markItemAsSold(int auctionId);
-    void markItemAsDelivered(int auctionId);
+    public void markItemAsForSale(int auctionId);
+    public void markItemAsSold(int auctionId);
+    public void markItemAsDelivered(int auctionId);
 
     // Gestion des comptes
-    void createAccount(UserAccount account);
-    void deleteAccount(String username);
-    UserAccount findAccountByUsername(String username);
-    void deactivateAccount(String username);
+    public void createUser(User user);
+    public void deleteUser(String username);
+    public User findUserByUsername(String username);
+    public void deactivateUser(String username);
+    
+    public List<User> getAllUsers();
+    public List<User> getAdminUsers();
+    public List<User> getRegularUsers();
 
     // Autres fonctionnalités
-    void cancelSaleById(int auctionId);
+    public void cancelSaleById(int auctionId);
 }
