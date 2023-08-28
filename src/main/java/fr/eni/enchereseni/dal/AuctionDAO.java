78f27de6a986e2f9f029696ab03d6c25fba8c261
UNiK <UNiK@UNIK-PSE06UQOK4.home>
2023-08-28 15:27:52 +0200

    AuctionDAO

--- src/main/java/fr/eni/enchereseni/dal/AuctionDAO.java
+++ src/main/java/fr/eni/enchereseni/dal/AuctionDAO.java
@@ -1,6 +1,12 @@
 package fr.eni.enchereseni.dal;
 
+import java.util.List;
+
+import fr.eni.enchereseni.bo.SoldItem;
+
 public interface AuctionDAO {
-	
+	public void insert(SoldItem item);
+	public List<SoldItem> getAll();
+	public SoldItem findByUser(int userID);
 
 }
