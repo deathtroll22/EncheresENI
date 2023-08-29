81154d9a90322bc9bc73bfc241769644aa2e9033
UNiK <UNiK@UNIK-PSE06UQOK4.home>
2023-08-29 17:33:29 +0200

    fin de journée et ça marche pas

---
+++ src/main/java/fr/eni/enchereseni/ihm/AccueilServlet.java
@@ -0,0 +1,45 @@
+package fr.eni.enchereseni.ihm;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import fr.eni.enchereseni.bll.AuctionManager;
+import fr.eni.enchereseni.bll.AuctionManagerSing;
+
+/**
+ * Servlet implementation class NolTestServlet2
+ */
+@WebServlet("/AccueilServlet")
+public class AccueilServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private AuctionManager manager = AuctionManagerSing.getInstance();
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AccueilServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/accueil.jsp").forward(request, response);
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- src/main/java/fr/eni/enchereseni/ihm/InsertUserServlet.java
+++ src/main/java/fr/eni/enchereseni/ihm/InsertUserServlet.java
@@ -7,8 +7,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.RequestDispatcher;
 import fr.eni.enchereseni.bll.AuctionManager;
+import fr.eni.enchereseni.bll.AuctionManagerSing;
 import fr.eni.enchereseni.bll.BLLException;
-
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,8 +16,32 @@ import java.util.List;
 
 @WebServlet("/InsertUserServlet")
 public class InsertUserServlet extends HttpServlet {
-	
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	request.getRequestDispatcher("/WEB-INF/insertUser.jsp").forward(request, response);
-    }
+	private static final long serialVersionUID = 1L;
+	private AuctionManager manager = AuctionManagerSing.getInstance();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/WEB-INF/insertUser.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		if (request.getParameter("Create_Account_Button") != null) {
+			doAdd(request, response);
+		} else if (request.getParameter("Cancel") != null) {
+			doFact(request, response);
+		}
+
+		request.getRequestDispatcher("/WEB-INF/biere.jsp").forward(request, response);
+	}
+
+	private void doFact(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    response.sendRedirect(request.getContextPath() + "/AccueilServlet");
+	}
+
+	private void doAdd(HttpServletRequest request, HttpServletResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
 }
