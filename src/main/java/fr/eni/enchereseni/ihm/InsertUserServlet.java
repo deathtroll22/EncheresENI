852a1cc9a78f1c3e0a55ba4248a4e7eda1a837ba
UNiK <UNiK@UNIK-PSE06UQOK4.home>
2023-08-30 09:03:28 +0200

    modif servlet encours

--- src/main/java/fr/eni/enchereseni/ihm/InsertUserServlet.java
+++ src/main/java/fr/eni/enchereseni/ihm/InsertUserServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.RequestDispatcher;
 import fr.eni.enchereseni.bll.AuctionManager;
 import fr.eni.enchereseni.bll.AuctionManagerSing;
 import fr.eni.enchereseni.bll.BLLException;
+import fr.eni.enchereseni.bo.User;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -26,6 +27,21 @@ public class InsertUserServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		User user = null;
+		
+		try {
+			String username = request.getParameter("pseudo");
+			String firstName = request.getParameter("first_name");
+			String lastName = request.getParameter("name");
+			String email = request.getParameter("mail");
+			String phoneNumber = request.getParameter("phone");
+			String street = request.getParameter("street");
+			String postalCode = request.getParameter("post_code");
+			String city = request.getParameter("city");
+			String password = request.getParameter("password");
+			String passwordconfirm = request.getParameter("confirmer_mot_de_passe");
+		}
 
 		if (request.getParameter("Create_Account_Button") != null) {
 			doAdd(request, response);
@@ -36,12 +52,5 @@ public class InsertUserServlet extends HttpServlet {
 		request.getRequestDispatcher("/WEB-INF/biere.jsp").forward(request, response);
 	}
 
-	private void doFact(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    response.sendRedirect(request.getContextPath() + "/AccueilServlet");
-	}
-
-	private void doAdd(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 }
