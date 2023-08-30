c8d62a9e8e42b5454f69000799e7567e4072803f
UNiK <UNiK@UNIK-PSE06UQOK4.home>
2023-08-30 10:45:52 +0200

    servlet suite

--- src/main/java/fr/eni/enchereseni/ihm/InsertUserServlet.java
+++ src/main/java/fr/eni/enchereseni/ihm/InsertUserServlet.java
@@ -28,9 +28,7 @@ public class InsertUserServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/insertUser.jsp");
-			
-		User user = null;
-		
+
 		try {
 			String username = request.getParameter("pseudo");
 			String firstName = request.getParameter("first_name");
@@ -42,68 +40,54 @@ public class InsertUserServlet extends HttpServlet {
 			String city = request.getParameter("city");
 			String password = request.getParameter("password");
 			String passwordconfirm = request.getParameter("confirmer_mot_de_passe");
-			
-			
-		
-		if(username.length()==0 || username.isEmpty() ) {
-			request.setAttribute("error", "please enter a username");
-			dispatcher.forward(request, response);
-		}
-		else if (firstName.length()==0 || firstName.isEmpty() ) {
-			request.setAttribute("error", "please enter a name");
-			dispatcher.forward(request, response);
-		}
-		else if (lastName.length()==0 || lastName.isEmpty() ) {
-			
-			request.setAttribute("error", "please enter a lastName");			
-			dispatcher.forward(request, response);
-		}
-		else if (email.length()==0 || email.isEmpty() ) {
-			request.setAttribute("error", "please enter a email");
-			dispatcher.forward(request, response);
-		}
-		else if (phoneNumber.length()==0 || phoneNumber.isEmpty() ) {
-			request.setAttribute("error", "please enter a phoneNumber");
-			dispatcher.forward(request, response);
-		}
-		else if (street.length()==0 || street.isEmpty() ) {
-			request.setAttribute("error", "please enter a street");
-			dispatcher.forward(request, response);
-		}
-		else if (postalCode.length()==0 || postalCode.isEmpty() ) {
-			request.setAttribute("error", "please enter a postal Code");
-			dispatcher.forward(request, response);
-		}
-		else if (city.length()==0 || city.isEmpty() ) {
-			request.setAttribute("error", "please enter a city");
-			dispatcher.forward(request, response);
-		}
-		else if (password.length()==0 || password.isEmpty() ) {
-			request.setAttribute("error", "please enter a password");
-			dispatcher.forward(request, response);
-		}
-		else if (passwordconfirm.equals(password)) {
-			
-			user = new User(username, lastName, firstName, email, phoneNumber, street, postalCode, city, passwordconfirm, null, false);
-			
-			
-			if (user != null) {
-				HttpSession session = request.getSession();
-				session.setAttribute("ConnectedUser", user);
-				
-				this.getServletContext().getRequestDispatcher("/accueilConnected").forward(request, response);
-			} 
-			else 
-			{
-				request.setAttribute("erreur", "Aucun utilisateur");
+
+			if (username.length() == 0 || username.isEmpty()) {
+				request.setAttribute("error", "please enter a username");
 				dispatcher.forward(request, response);
+			} else if (firstName.length() == 0 || firstName.isEmpty()) {
+				request.setAttribute("error", "please enter a name");
+				dispatcher.forward(request, response);
+			} else if (lastName.length() == 0 || lastName.isEmpty()) {
+
+				request.setAttribute("error", "please enter a lastName");
+				dispatcher.forward(request, response);
+			} else if (email.length() == 0 || email.isEmpty()) {
+				request.setAttribute("error", "please enter a email");
+				dispatcher.forward(request, response);
+			} else if (phoneNumber.length() == 0 || phoneNumber.isEmpty()) {
+				request.setAttribute("error", "please enter a phoneNumber");
+				dispatcher.forward(request, response);
+			} else if (street.length() == 0 || street.isEmpty()) {
+				request.setAttribute("error", "please enter a street");
+				dispatcher.forward(request, response);
+			} else if (postalCode.length() == 0 || postalCode.isEmpty()) {
+				request.setAttribute("error", "please enter a postal Code");
+				dispatcher.forward(request, response);
+			} else if (city.length() == 0 || city.isEmpty()) {
+				request.setAttribute("error", "please enter a city");
+				dispatcher.forward(request, response);
+			} else if (password.length() == 0 || password.isEmpty()) {
+				request.setAttribute("error", "please enter a password");
+				dispatcher.forward(request, response);
+			} else if (passwordconfirm.equals(password)) {
+
+				User connectedUser = new User(username, lastName, firstName, email, phoneNumber, street, postalCode,
+						city, passwordconfirm, null, false);
+
+				if (connectedUser != null) {
+					HttpSession session = request.getSession();
+					session.setAttribute("connectedUser", connectedUser);
+
+					this.getServletContext().getRequestDispatcher("/accueil").forward(request, response);
+				} else {
+					request.setAttribute("error", "no user");
+					dispatcher.forward(request, response);
+				}
+
 			}
-			
-			
-			
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
-		}
+
 	}
 }
-		
-
